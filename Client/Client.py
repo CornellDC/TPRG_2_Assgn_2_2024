f88a5396ab0796c121dae0fd8575920b2fdcddd9
CornellDC <cornell.falconerlawson@dcmail.ca>
2024-11-28 15:51:48 -0500

    Update Client.py
    Client will now parse the JSON object received, and print each key and value pair to the console.
    
    Also updated the IP address as it changed.

--- Client/Client.py
+++ Client/Client.py
@@ -2,11 +2,17 @@
 # The client
 
 import socket
-s = socket. socket()
-host = '192.168.2.33'# ip of raspberry pi, running the server
+s = socket.socket()
+host = '10.102.13.55'# ip of raspberry pi, running the server
 port = 5000
 s.connect((host, port))
 message = s.recv(1024).decode()
-print(message)
-s.close()
+# print(message)
 
+f_dict = eval(message)  # The eval() function evaluates JavaScript code represented as a string and returns its completion value.
+
+# Print each key and value pair to the shell.
+for key, value in f_dict.items(): # https://stackoverflow.com/a/5905166
+    print(f'{key} = {value}')
+
+s.close()
